18887154651de2736f3b8dcbefa7dad7b83657dd
BaxtiyorXusanov <xusanovbaxtiyor07>
2021-10-13 22:50:34 +0500

    commit

--- js/script.js
+++ js/script.js
@@ -1,28 +1,32 @@
-const elList = document.querySelector('.menu');
-const elInput = document.querySelector('#input');
-const elBtn = document.querySelector('#btn');
+// const elList = document.querySelector('.menu');
+// const elInput = document.querySelector('#input');
+// const elBtn = document.querySelector('#btn');
 
 
-elBtn.addEventListener('click', () =>{
-    let newLi = document.createElement('li')
-    let newTextArea = document.createElement('p')
-    let newCheckbox = document.createElement('input')
-    let newBtn = document.createElement('button')
+// elBtn.addEventListener('click', () =>{
+//     let newLi = document.createElement('li')
+//     let newTextArea = document.createElement('p')
+//     let newCheckbox = document.createElement('input')
+//     let newBtn = document.createElement('button')
 
-    newLi.classList.add('item')
-    newTextArea.classList.add('item__text')
-    newCheckbox.classList.add('item__checkbox')
-    newCheckbox.setAttribute("type", "checkbox")
-    newBtn.classList.add('item__btn', 'fas', 'fa-trash-alt', 'fa-1x')
-    newBtn.setAttribute("type", "button")
+//     newLi.classList.add('item')
+//     newTextArea.classList.add('item__text')
+//     newCheckbox.classList.add('item__checkbox')
+//     newCheckbox.setAttribute("type", "checkbox")
+//     newBtn.classList.add('item__btn', 'fas', 'fa-trash-alt', 'fa-1x')
+//     newBtn.setAttribute("type", "button")
 
-    newTextArea.textContent = elInput.value.trim()
+//     newTextArea.textContent = elInput.value.trim()
 
-    newLi.appendChild(newCheckbox)
-    newLi.appendChild(newTextArea)
-    newLi.appendChild(newBtn)
+//     newLi.appendChild(newCheckbox)
+//     newLi.appendChild(newTextArea)
+//     newLi.appendChild(newBtn)
 
-    console.log(newLi)
+//     console.log(newLi) 
 
-    elList.appendChild(newLi)
-})
+//     elList.appendChild(newLi)
+// })
+
+let key = '58ed228a';
+
+fetch 
